b8da2265ee7b94f34ff1d0b568709577830f597f
N1Coder <tomofanteng123@gmail.com>
2023-07-10 09:53:17 +0800

    - add .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,19 +4,14 @@ import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBovQkTluB-QSt7TS9fGr2iuSePtldRROE",
-  authDomain: "noteballs-app-fe8f8.firebaseapp.com",
-  projectId: "noteballs-app-fe8f8",
-  storageBucket: "noteballs-app-fe8f8.appspot.com",
-  messagingSenderId: "686177755023",
-  appId: "1:686177755023:web:691990bb775787856975fa",
-  measurementId: "G-5FN7T1JZFL",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
